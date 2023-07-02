1b1c6de18beffb626f72cccb5b4588cf5d36729b
ZOMini <zarikacr@gmail.com>
2023-07-02 22:30:15 +0300

    v1.3

--- monitor/app.py
+++ monitor/app.py
@@ -1,6 +1,7 @@
 import logging
 
 from flask import Response, request
+from web.pages import pages
 
 from api.v1.auth import auth
 from api.v1.monitor import monitor
@@ -10,6 +11,7 @@ from services.jwt_service import *  # Регистрируем JWT
 
 app.register_blueprint(auth, url_prefix='/api/v1/auth')
 app.register_blueprint(monitor, url_prefix="/api/v1/monitor")
+app.register_blueprint(pages, url_prefix="/web")
 init_docs()
 
 
--- monitor/web/pages.py
+++ monitor/web/pages.py
@@ -1,15 +1,54 @@
+import logging
 
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    redirect,
+    render_template,
+    render_template_string,
+    request,
+    url_for
+)
 from flask_jwt_extended import jwt_required
+from sqlalchemy import select
 
 from core.config import settings
+from db.connection_db import db_session
+from db.models_db import Link
 from services.web_monitor_service import WebMonitorService
 
 pages = Blueprint('pages', __name__)
 
 
-@pages.route('/new_urls', methods=['GET', 'POST'])
-@jwt_required(optional=settings.app.jwt.disabled_in_api)
-def new_urls():
-    response = WebMonitorService.post_one_link()
-    return response
+@pages.route('/')
+def index():
+    links = db_session.scalars(select(Link)).all()
+    db_session.close()
+    return render_template('index.html', links=links)
+
+@pages.route('/<string:link_id>')
+def get_link(link_id):
+    link = db_session.scalar(select(Link).filter(Link.id == link_id).limit(1))
+    db_session.close()
+    return render_template('link.html', link=link)
+
+@pages.route('/new_link', methods=['GET', 'POST'])
+def new_link():
+    if request.method == 'POST':
+        # logging.getLogger('console').info('Url - %s error else.', request.files)
+        try:
+            if 'url' in request.form:
+              logging.getLogger('console').info('Url - %s error else.', 'AAAAAAAAAAAAAAA')
+              # logging.getLogger('console').info('Url - %s error add.', request.form['url'])
+              link_obj = Link(request.form['url'])
+              db_session.add(link_obj)
+              db_session.commit()
+            elif 'file' in request.files:
+              # logging.getLogger('console').info('Url - %s error else.', request.files['file'])
+              result = WebMonitorService.post_links()
+              return render_template_string(str(result))
+        except Exception as e:
+            logging.getLogger('console').info('Url - %s error add.', e.args)
+            db_session.rollback()
+            return render_template_string(str(e.args))
+        return redirect(url_for('pages.index'))
+    return render_template('add_links.html')
